838f6440d8ca424f63ebce56f4baeb954f75e40c
gsk44-1 <gsk44@case.edu>
2026-09-01 21:19:21 -0400

    dim label bug 2

--- RandPatchDataset.py
+++ RandPatchDataset.py
@@ -27,7 +27,7 @@ class RandPatchDataset(Dataset):
         return self.samples_per_epoch
 
     def __getitem__(self, idx):
-        H, W = self.labels.shape[1:]
+        _, H, W = self.labels.shape
         p = self.patch_size
         b = self.block_size
     
